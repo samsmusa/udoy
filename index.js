a13d202fea6b77976430f9b5ec7d1fa39e652ce5
Muhammad <samsmusa@outlook.com>
2022-06-15 12:29:34 +0600

    'Change'

--- index.js
+++ index.js
@@ -173,7 +173,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Running server");
+  res.send("Running udoylab server");
 });
 
 app.listen(port, () => {
